ed053d92466c225554ad117cb3510bf837d127ac
hodeano <hodeanodixon@gmail.com>
2021-09-24 14:08:53 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom";
-
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
@@ -12,9 +10,9 @@ Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
-<Router>
+
     <App />
-</Router>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
